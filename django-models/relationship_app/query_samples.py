2a914b33e284f99cfd1d44a6c27396c515e9a6c3
reed <hsfarid08@gmail.com>
2024-09-01 23:19:16 +0000

    modify query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def get_books_by_author(author_name):
     try:
-        author = Author.objects.get(name=author_name).objects.filter(name=author_name)
+        author = Author.objects.get(name=author_name).objects.filter(name=author)
         books = author.books.all()  # Using the related_name 'books'
         return books
     except Author.DoesNotExist:
