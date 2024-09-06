bb818c03fcaeceffd0815a55f125715a2e4be9ad
reed <hsfarid08@gmail.com>
2024-09-06 03:56:12 +0000

    modify relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,7 @@ from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 #CBV that displays details of a specific library
 class LibraryDetailView(DetailView):
