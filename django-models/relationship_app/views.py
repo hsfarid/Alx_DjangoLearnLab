12972e1b5121ef7beddfd7b889c5021cbc35e7de
reed <hsfarid08@gmail.com>
2024-09-07 02:34:30 +0000

    modify relationship_app.views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
